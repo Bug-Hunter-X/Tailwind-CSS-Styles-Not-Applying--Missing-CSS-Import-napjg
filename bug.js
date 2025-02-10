cef10f79897ea2e92e41ee45c9e177e0d266b923
BugHunter-X <askzenai@gmail.com>
2025-02-10 02:16:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// This code uses Tailwind classes but doesn't import the CSS file.
+<div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Button
+</div>
+```
